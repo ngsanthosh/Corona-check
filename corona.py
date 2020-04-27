c71e025a97e25fb34d5cc24fd10db629689b3d67
N.G. Santhosh Sudhaan <63006841+ngsanthosh@users.noreply.github.com>
2020-04-27 18:26:55 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -2,6 +2,7 @@
 #send your feedback to santhoshsudhaan@yahoo.com
 
 def yep():
+
     print("\nPlease let me know your current temperature in degree Fahrenheit:")
     temp=int(input("\n1.Normal(96F-98.6F)\n2.Fever(98.6F-102F)\n3.High Fever(>102F)\n4.Don't know\n"))
     if(temp==1 or temp==2 or temp==3 or temp==4):
@@ -11,6 +12,7 @@ def yep():
         yep();
 
 def trav():
+
     print("\nNow please select your travel and exposure details:")
     tra=int(input("\n1.No Travel History\n2.No contact with anyone with Symptoms\n3.History of abroad travel in last 14 days\n"))
     if tra==1 or tra==2:
@@ -21,9 +23,12 @@ def trav():
         print("\nWrong Entry!, Sorry start from the beginning!")
         yep();
         
+
 def symp():
+
     global ris,symptoms,g;
     ris=0;
+    
     print("\nAre you experiencing any of the symptoms below?")
     symptoms=["Dry cough","Sore throat","Weakness","Change in Appetite","Difficulty in Breathing","Drowsiness","none of these"]
     for i in range(len(symptoms)):
@@ -41,6 +46,7 @@ def symp():
         yep();
           
 def hist():
+
     print("\nDo you have any of these conditions?")
     his=int(input("\n1.diabetes\n2.High Blood Pressure\n3.Heart disease\n4.Lung diseases\n5.None of these\n"));
     if(his==1 or his==2 or his==3 or his==4):
@@ -56,21 +62,28 @@ def hist():
     
     
 def good():
+
     print("\nThat's good., You are all fine!")
 def final():
+
     print("Seems Risky.., It is recommmended to have a physical test at nearby hospital!")
     
 
 intr="Hello and Welcome to Coronavirus Self-Assess Test!"
 intr2="Created by Santhosh"
+
 print(intr.center(75,"*"))
 print(intr2.center(75,"~"))
+
 a=input("\nPlease enter your name: ")
 name=a.capitalize()
+
 print("\nHello, {}.. here I have created a assessment scan., Do answer the questions honestly!".format(name))
 print("So, lets get started!")
 print("\nNOTE: For Multi choice questions, Reply your answers with respective numbers (i.e., 1,2..etc.)")
+
 age=int(input("\nHow old are you? "))
+
 if(age>12):
     gen=int(input("\nplease select your gender:\n1.Male\n2.Female\n3.Others\n"))
     if(gen==1 or gen==2 or gen==3):
@@ -79,6 +92,7 @@ if(age>12):
         print("\nWrong Entry!, Sorry start from the beginning!")
 else:
     print("\nSorry! This Assess test suits only for the agers above 12.");
+    
     
 print("\nThanks for your time, {}..\n\nProgrammed by Santhosh ~ VCET IT 1st year.".format(name));
 print("\nSTAY HOME! STAY SAFE!")
